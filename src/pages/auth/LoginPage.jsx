dc5f4aa394a242704c7ee627819b5e8c0f11fa02
JIMIN HWANG <hwang0010@naver.com>
2024-11-27 21:55:14 +0900

    feat: 로그인 페이지 상태 관리

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -1,20 +1,45 @@
 import '../../styles/auth/LoginPage.css';
+import React, { useState } from 'react';
 
 function LoginPage() {
+  const [formData, setFormData] = useState({ email: '', password: '' });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target; 
+    setFormData({ ...formData, [name]: value }); 
+  };
+
+  const handleLogin = () => {
+    console.log('로그인 데이터:', formData);
+ 
+  };
+
   return (
     <div className="Login-container">
       <h1>로그인</h1>
       <div className="Login-input-area">
         <label>이메일</label>
-        <input type="text" placeholder="이메일을 입력해주세요."></input>
+        <input
+          type="text"
+          name="email" 
+          placeholder="이메일을 입력해주세요."
+          value={formData.email}
+          onChange={handleChange}
+        />
       </div>
       <div className="Login-input-area">
         <label>비밀번호</label>
-        <input type="password" placeholder="비밀번호를 입력해주세요."></input>
+        <input
+          type="password"
+          name="password"
+          placeholder="비밀번호를 입력해주세요."
+          value={formData.password}
+          onChange={handleChange}
+        />
       </div>
-      <div className="Login-button">
-        로그인
-      </div>
+        <button className="Login-button" onClick={handleLogin}>
+          로그인
+        </button>
     </div>
   );
 }
