e0fa7007105091dc258b9c7a35cd365677662b63
mk-star <ssunn0812@naver.com>
2024-12-26 19:06:26 +0900

    feat: 대기열 진입 경로 추가

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import { Routes, Route } from 'react-router-dom';
 import Header from "./components/Header";
 import MainPage from "./pages/main/MainPage";
 import MyPage from "./pages/my/MyPage";
-import Reservation from "./pages/Reservation/Reservation"
-import ReservationCompleted from './pages/Reservation/ReservationCompleted';
+import Reservation from "./pages/reservation/Reservation"
+import ReservationCompleted from './pages/reservation/ReservationCompleted';
+import WaitingRoom from "./pages/reservation/WaitingRoom";
 
 import { requestPermission } from './fireabse/firebaseConfig';
 
@@ -18,8 +19,9 @@ const App = () => {
           <Routes>
               <Route path="/" element={<MainPage />} />
               <Route path="/mypage" element={<MyPage />} />
-              <Route path="/reservation" element={<Reservation />} />
+              <Route path="/reservation/:festivalId" element={<Reservation />} />
               <Route path="/reservation/completed" element={<ReservationCompleted />} />
+              <Route path="/waiting-room/:festivalId" element={<WaitingRoom />} />
           </Routes>
       </>
   );
