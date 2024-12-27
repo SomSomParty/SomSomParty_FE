92ee93ff2931fa72e077bdb4c3ec72141d425a97
JIMIN HWANG <hwang0010@naver.com>
2024-12-27 21:07:09 +0900

    feat: AuthContext 추가
    - 회원 관리에 필요한 변수들을 관리하기 위한 AuthContext 추가.

--- src/App.js
+++ src/App.js
@@ -5,25 +5,32 @@ import MainPage from "./pages/main/MainPage";
 import MyPage from "./pages/my/MyPage";
 import Reservation from "./pages/Reservation/Reservation"
 import ReservationCompleted from './pages/Reservation/ReservationCompleted';
-import LoginPage from './pages/auth/LoginPage'
+import SignInPage from './pages/auth/SignInPage';
+import SignUpPage from './pages/auth/SignUpPage';
+
+import { AuthProvider } from './context/AuthContext';
 
 import { requestPermission } from './fireabse/firebaseConfig';
 
 const App = () => {
   useEffect(() => {
-    requestPermission();
+    requestPermission(); // 알림 권한 요청
   }, []);
+
   return (
-      <>
+    <>
+      <AuthProvider>
         <Header />
           <Routes>
               <Route path="/" element={<MainPage />} />
               <Route path="/mypage" element={<MyPage />} />
               <Route path="/reservation" element={<Reservation />} />
               <Route path="/reservation/completed" element={<ReservationCompleted />} />
-              <Route path="/login" element={<LoginPage />} />
+              <Route path="/signin" element={<SignInPage />} />
+              <Route path="/signup" element={<SignUpPage />} />
           </Routes>
-      </>
+      </AuthProvider>
+    </>
   );
 };
 
