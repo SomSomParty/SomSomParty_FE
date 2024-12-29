b569e9c0f3f918616e35836d64b245f22a68c3ca
이승연 <dltmddus9082@naver.com>
2024-12-29 21:22:28 +0900

    fix: 오류 수정

--- src/pages/my/MyPage.jsx
+++ src/pages/my/MyPage.jsx
@@ -1,6 +1,5 @@
-import React, {useContext, useEffect, useState} from 'react';
 import { useNavigate } from 'react-router-dom';
-import React, {useEffect, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import CardList from '../../components/CardList';
 import axios from 'axios';
 import '../../styles/CardList.css';
