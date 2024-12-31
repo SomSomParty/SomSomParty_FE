5ef423abba91a93293bfea2e1134505f8379e9e0
mk-star <ssunn0812@naver.com>
2024-12-31 14:07:14 +0900

    feat:  내 예약 목록, 축제 상세 페이지 연결

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,55 +1,62 @@
-import React from 'react';
-import '../styles/CardList.css';
+import React from "react";
+import "../styles/CardList.css";
 
-const Card = ({ id, name, startDate, endDate, reservationDate, festivalDate, onEventClick }) => {
-    return (
-        <div className="card" onClick={() => onEventClick(id)}>
-            <div className = "card-image">
-                {/*<img src = "image_url" alt = "image description" />*/}
-                {/*<span>사진</span>*/}
-            </div>
-            <div className = "card-content">
-                {startDate && endDate ? (
-                    <>
-                        <span className="event-date">{startDate} ~ </span>
-                        <span className="event-date">{endDate}</span>
-                    </>
-                ) : (
-                    <>
-                        <span className="event-date">축제일: {festivalDate}</span><br/>
-                        <span className="event-date">예매일: {reservationDate}</span>
-                    </>
-                )}
-                <p className = "event-name">{name}</p>
-            </div>
-        </div>
-    );
+const Card = ({
+  id,
+  name,
+  startDate,
+  endDate,
+  reservationDate,
+  festivalDate,
+  onEventClick,
+}) => {
+  return (
+    <div className="card" onClick={() => onEventClick(id)}>
+      <div className="card-image">
+        {/*<img src = "image_url" alt = "image description" />*/}
+        {/*<span>사진</span>*/}
+      </div>
+      <div className="card-content">
+        {startDate && endDate ? (
+          <>
+            <span className="event-date">{startDate} ~ </span>
+            <span className="event-date">{endDate}</span>
+          </>
+        ) : (
+          <>
+            <span className="event-date">축제일: {festivalDate}</span>
+            <br />
+            <span className="event-date">예매일: {reservationDate}</span>
+          </>
+        )}
+        <p className="event-name">{name}</p>
+      </div>
+    </div>
+  );
 };
 
 const CardList = ({ events, onEventClick }) => {
-    console.log('CardList received onEventClick:', onEventClick);
-    return (
-        <div className="card-list">
-            {events && events.length > 0 ? (
-                events.map((event) => (
-                    <div key={event.id}>
-                        <Card
-                            id={event.id}
-                            name={event.name}
-                            startDate={event.startDate}
-                            endDate={event.endDate}
-                            reservationDate={event.reservationDate}
-                            festivalDate={event.festivalDate}
-                            onEventClick={onEventClick} // onEventClick 전달
-                        />
-                    </div>
-                ))
-            ) : (
-                <p>결과가 없습니다.</p>
-            )}
-        </div>
-    );
+  return (
+    <div className="card-list">
+      {events && events.length > 0 ? (
+        events.map((event) => (
+          <div key={event.id}>
+            <Card
+              id={event.festivalId}
+              name={event.name}
+              startDate={event.startDate}
+              endDate={event.endDate}
+              reservationDate={event.reservationDate}
+              festivalDate={event.festivalDate}
+              onEventClick={onEventClick} // onEventClick 전달
+            />
+          </div>
+        ))
+      ) : (
+        <p>결과가 없습니다.</p>
+      )}
+    </div>
+  );
 };
-
 
 export default CardList;
--- src/pages/my/MyPage.jsx
+++ src/pages/my/MyPage.jsx
@@ -1,89 +1,105 @@
-import { useNavigate } from 'react-router-dom';
-import React, {useContext, useEffect, useState} from 'react';
-import CardList from '../../components/CardList';
-import axios from 'axios';
-import '../../styles/CardList.css';
-import '../../styles/my/MyPage.css';
-import {AuthContext} from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import CardList from "../../components/CardList";
+import axios from "axios";
+import "../../styles/CardList.css";
+import "../../styles/my/MyPage.css";
+import { AuthContext } from "../../context/AuthContext";
 
 const MyPage = () => {
-    const [events, setEvents] = useState([]);
-    const [error, setError] = useState(null);
-    const [lastId, setLastId] = useState(0);
-    const [limit] = useState(8);
-    const [hasMore, setHasMore] = useState(true);
-    const navigate = useNavigate();
-    const {accessToken} = useContext(AuthContext);
+  const [events, setEvents] = useState([]);
+  const [error, setError] = useState(null);
+  const [lastId, setLastId] = useState(0);
+  const [limit] = useState(8);
+  const [hasMore, setHasMore] = useState(true);
+  const navigate = useNavigate();
+  const { accessToken } = useContext(AuthContext);
 
-    // 예약 목록 가져오기
-    const fetchReservationList = async () => {
-        setError(null);
-        if (!hasMore) return;
+  // 예약 목록 가져오기
+  const fetchReservationList = async () => {
+    setError(null);
+    if (!hasMore) return;
 
-        try {
-            const response = await axios.get('/api/reservations', {
-                params: {
-                    lastId: lastId,
-                    limit: limit,
-                },
-                headers: {
-                    Authorization: `Bearer ${accessToken}`,
-                },
-            });
+    try {
+      const response = await axios.get("/api/reservations", {
+        params: {
+          lastId: lastId,
+          limit: limit,
+        },
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
 
-            const { reservations, hasNext } = response.data;
+      const { reservations, hasNext } = response.data;
 
-            setEvents((prevEvents) => [
-                ...prevEvents,
-                ...reservations.map((reservation) => ({
-                    id: reservation.id,
-                    name: reservation.festivalInfo.name,
-                    reservationDate: `${reservation.reservationDate[0]}.${String(reservation.reservationDate[1]).padStart(2, '0')}.${String(reservation.reservationDate[2]).padStart(2, '0')}`,
-                    festivalDate: `${reservation.festivalDate[0]}.${String(reservation.festivalDate[1]).padStart(2, '0')}.${String(reservation.festivalDate[2]).padStart(2, '0')}`
-                })),
-            ]);
+      setEvents((prevEvents) => [
+        ...prevEvents,
+        ...reservations.map((reservation) => ({
+          id: reservation.id,
+          name: reservation.festivalInfo.name,
+          festivalId: reservation.festivalInfo.id,
+          reservationDate: `${reservation.reservationDate[0]}.${String(
+            reservation.reservationDate[1]
+          ).padStart(2, "0")}.${String(reservation.reservationDate[2]).padStart(
+            2,
+            "0"
+          )}`,
+          festivalDate: `${reservation.festivalDate[0]}.${String(
+            reservation.festivalDate[1]
+          ).padStart(2, "0")}.${String(reservation.festivalDate[2]).padStart(
+            2,
+            "0"
+          )}`,
+        })),
+      ]);
 
-            setLastId(response.data.lastId != null ? response.data.lastId : reservations[reservations.length - 1].id);
-            setHasMore(hasNext);
-        } catch (err) {
-            setError('예약 목록을 불러오는 중 문제가 발생했습니다.');
-        }
-    };
-
-    useEffect(() => {
+      setLastId(
+        response.data.lastId != null
+          ? response.data.lastId
+          : reservations[reservations.length - 1].id
+      );
+      setHasMore(hasNext);
+    } catch (error) {
+      if (error.response.data == "토큰 검증에 실패했습니다.") {
+        reissueAccessToken();
         fetchReservationList();
-    }, []);
+      }
+      setError("예약 목록을 불러오는 중 문제가 발생했습니다.");
+    }
+  };
 
-    // 카드 클릭 시 이벤트 처리
-    const handleEventClick = (eventId) => {
-        navigate(`/festival?id=${eventId}`); // `/festival` 경로로 이동
-    };
+  useEffect(() => {
+    fetchReservationList();
+  }, []);
 
-    return (
-        <div className="my-page-container">
-            <div className="my-page-header">
-                <p
-                    className="my-page-link"
-                    onClick={() => navigate('/chat')}
-                >
-                    내 채팅방
-                </p>
-                <p className="my-page-title">내 예약 조회</p>
-            </div>
-            {events.length > 0 ? (
-                <>
-                    <CardList events={events} />
-                    {hasMore && (
-                        <button className="more-button" onClick={fetchReservationList}>
-                            더 보기
-                        </button>
-                    )}
-                </>
-            ) : (
-                <p>예약 내역이 없습니다.</p>
-            )}
-        </div>
-    );
+  // 카드 클릭 시 이벤트 처리
+  const handleEventClick = (eventId) => {
+    navigate(`/festival-detail/${eventId}`); // `/festival` 경로로 이동
+  };
+
+  return (
+    <div className="my-page-container">
+      <div className="my-page-header">
+        <p className="my-page-link" onClick={() => navigate("/chat")}>
+          내 채팅방
+        </p>
+        <p className="my-page-title">내 예약 조회</p>
+      </div>
+      {events.length > 0 ? (
+        <>
+          <CardList events={events} onEventClick={handleEventClick} />
+          {hasMore && (
+            <button className="more-button" onClick={fetchReservationList}>
+              더 보기
+            </button>
+          )}
+        </>
+      ) : (
+        <p>예약 내역이 없습니다.</p>
+      )}
+    </div>
+  );
 };
 
 export default MyPage;
