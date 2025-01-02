7b9aeeb9c41705176cd6bd826569c955dfe4014a
이가연 <99721126+dlrkdus@users.noreply.github.com>
2025-01-03 04:18:35 +0900

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: 'http://somparty-loadbalancer-2037870297.ap-northeast-2.elb.amazonaws.com', // 로드밸런서 DNS
+      target: 'http://somparty-alb-1442998736.ap-northeast-2.elb.amazonaws.com', // 로드밸런서 DNS
       pathRewrite: {
         '^/api': ''
       },
@@ -16,7 +16,7 @@ module.exports = function(app) {
   app.use(
     "/ws-chat",
     createProxyMiddleware({
-      target: 'http://somparty-loadbalancer-2037870297.ap-northeast-2.elb.amazonaws.com', // 로드밸런서 DNS
+      target: 'http://somparty-alb-1442998736.ap-northeast-2.elb.amazonaws.com', // 로드밸런서 DNS
       ws: true, // 웹소켓 사용
       changeOrigin: true,
     })
