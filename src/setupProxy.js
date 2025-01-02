2d1f891287b185c17ae38f1c23993aa3c787323b
이가연 <99721126+dlrkdus@users.noreply.github.com>
2025-01-02 19:35:24 +0900

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,24 +1,24 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function(app){
-    app.use(
-        "/api",
-        createProxyMiddleware( {
-            target: 'http://localhost:8080',
-            pathRewrite: {
-                '^/api':''
-            },
-            changeOrigin: true,
-        })
-    )
-      // 웹소켓 프록시 설정
+module.exports = function(app) {
   app.use(
-    "/ws-chat",
+    "/api",
     createProxyMiddleware({
-      target: "http://localhost:8080", // 웹소켓 서버 주소
-      ws: true, // 웹소켓을 사용할 수 있도록 설정
+      target: 'http://somparty-loadbalancer-2037870297.ap-northeast-2.elb.amazonaws.com', // 로드밸런서 DNS
+      pathRewrite: {
+        '^/api': ''
+      },
       changeOrigin: true,
     })
   );
-
+  
+  // 웹소켓 프록시 설정
+  app.use(
+    "/ws-chat",
+    createProxyMiddleware({
+      target: 'http://somparty-loadbalancer-2037870297.ap-northeast-2.elb.amazonaws.com', // 로드밸런서 DNS
+      ws: true, // 웹소켓 사용
+      changeOrigin: true,
+    })
+  );
 };
