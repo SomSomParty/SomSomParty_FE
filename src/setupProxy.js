e9d745136c185f6c76e8a7faa3b3eb553ecc0cbc
이가연 <99721126+dlrkdus@users.noreply.github.com>
2025-01-03 15:20:53 +0900

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -11,14 +11,5 @@ module.exports = function(app) {
       changeOrigin: true,
     })
   );
-  
-  // 웹소켓 프록시 설정
-  app.use(
-    "/ws-chat",
-    createProxyMiddleware({
-      target: 'https://somsomparty.store', // 로드밸런서 DNS https 적용
-      ws: true, // 웹소켓 사용
-      changeOrigin: true,
-    })
-  );
+
 };
